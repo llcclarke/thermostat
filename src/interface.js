199e4a579cdab546d0aba53ef415c65a8640eb4e
Matthew Ward <iammatthewward@gmail.com>
2016-05-12 18:16:37 +0100

    Some changes made

--- src/interface.js
+++ src/interface.js
@@ -23,13 +23,19 @@ $( document ).ready(function() {
     updateTemperature();
   });
 
+  $('#on-off').text(' on');
+
+  $('#temperature-power-save').click(function(){
+    if (('#on-off').text === ' on') {
+      $('#on-off').text(' off');}
+  });
+
   function updateTemperature(){
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyMonitor());
   }
 
-
-$.get("http://api.openweathermap.org/data/2.5/weather?q=London&mode=json&units=metric&APPID=94970d99a48188e387d14446d0b9d033", function(data){
-  $('#city-temp').text(data.main.temp);
+  $.get("http://api.openweathermap.org/data/2.5/weather?q=London&mode=json&units=metric&APPID=94970d99a48188e387d14446d0b9d033", function(data){
+    $('#city-temp').text(data.main.temp);
   });
 });
